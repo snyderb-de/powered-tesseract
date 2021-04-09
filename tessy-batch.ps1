f714c0b5f83f900bf8d500862f98c3be1905ddfe
Bryan Snyder <snyderb/de@gmail.com>
2021-04-09 15:25:52 -0400

    v1 only TIF

---
+++ tessy-batch.ps1
@@ -0,0 +1,9 @@
+﻿Set-Location '~\Scan'
+Get-ChildItem -Filter '*.tif'| ForEach-Object {
+    & '~\AppData\Local\Programs\Tesseract-OCR\tesseract.exe' -l eng $_.Name ($_.BaseName) pdf
+}
+Set-Location '~\'
+
+$wshell = New-Object -ComObject Wscript.Shell 
+
+$Output = $wshell.Popup("Your files have been converted and OCR'd!")
